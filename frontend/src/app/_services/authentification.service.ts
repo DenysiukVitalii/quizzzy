c90457f462de813c6c4776af0f515b77b8344d04
DenysiukVitalii <dendoc97@gmail.com>
2017-10-30 18:22:39 +0200

    fixing for linter

--- frontend/src/app/_services/authentification.service.ts
+++ frontend/src/app/_services/authentification.service.ts
@@ -1,27 +1,29 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, Response, RequestOptions } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class AuthenticationService {
     constructor(private http: Http) { }
 
     login(username: string, password: string, role: string) {
-        let headers = new Headers({"Content-Type": "application/json"});
-        let options = new RequestOptions({ headers: headers });
+        const headers = new Headers({'Content-Type': 'application/json'});
+        const options = new RequestOptions({ headers: headers });
 
-        return this.http.post('http://localhost:8081/login', JSON.stringify({ username: username, password: password, role: role }), options)
+        return this.http.post('http://localhost:8081/login',
+                              JSON.stringify({ username: username,
+                                               password: password,
+                                               role: role }), options)
             .map((response) => {
                 // login successful if there's a jwt token in the response
                 console.log(response);
-                let user = response.json();
+                const user = response.json();
                 console.log(user);
                 if (user && user.token) {
                     // store user details and jwt token in local storage to keep user logged in between page refreshes
                     localStorage.setItem('currentUser', JSON.stringify(user));
                 }
-                
                 return user;
             });
     }
